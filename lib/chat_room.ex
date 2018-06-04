f8b56dceafe16a40365937703ae8f8d9b494c1cf
AlexZerr <thwarts@gmail.com>
2018-06-03 20:17:53 -0600

    missed add

--- lib/chat_room.ex
+++ lib/chat_room.ex
@@ -4,4 +4,15 @@ defmodule ElixirChat.ChatRoom do
 
   end
 
+  def create(params) do
+    with chat_room_changeset <- Schema.create_changeset(params),
+      {:ok, chat_room} <- Query.insert(chat_room_changeset)
+    do
+      {:ok, customer}
+    else
+      {:error, %Ecto.Changeset{} = error} -> {:error, Query.changeset_errors(error)}
+      error -> error
+    end
+  end
+
 end
